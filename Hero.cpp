a28406915bd0e6317c73985fe7287b42a4aef6a4
Noora Panahi <nourap064@gmail.com>
2025-06-08 14:43:49 -0700

    working on Hero classes

--- Hero.cpp
+++ Hero.cpp
@@ -4,11 +4,11 @@
 
 using namespace std ; 
 
-Hero::Hero(std::string name , int MaxActions , std::string StartingLocation){
-    name = name ; 
-    MaxActions = MaxActions ; 
-    currentLocation = StartingLocation ; 
-}
+Hero::Hero( std::string name , int MaxActions , std::string StartingLocation , std::string specialAction)
+: name(name) , MaxActions(MaxActions) , currentLocation(StartingLocation) , specialAction(specialAction){}
+
+Hero::Hero(){}
+
 std::string Hero::GetName()const{
     return name ; 
 }
@@ -24,22 +24,19 @@ std::string Hero::GetSpecialActionInfo() const{
 std::string Hero::GetCurrentLocation() const{
     return currentLocation ; 
 }
-void Hero::SetName(const std::string& newName){
-    name = newName ; 
-}
 void Hero::SetMaxActions(int newMax){
     MaxActions = newMax ; 
 }
-void Hero::SetRemainingActions(int newRemaining){
-    if(newRemaining >= 0 && newRemaining <= MaxActions )
-        RemainingActions = newRemaining ; 
-    //else 
-        //throw invalid_argument(" ") ; 
-}
-void Hero::SetCurrentLocation(const std::string& location){
+void Hero::SetCurrentLocation(std::string location){
     currentLocation = location ;
 }
 void Hero::MoveTo(std::string Newlocation){
     currentLocation = Newlocation ; 
     cout << GetName() << " moved to " << GetCurrentLocation() << endl ;
+}
+std::ostream& operator << (std::ostream &out , const Hero &hero){
+    out << "Hero Name: " << hero.GetName() << endl  ; 
+    out << "location: " << hero.GetCurrentLocation() << endl  ; 
+    out << "actions: " << hero.GetSpecialActionInfo() << endl  ;  
+    return out ; 
 }
