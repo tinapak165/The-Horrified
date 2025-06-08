84600183750260cb9db6a2c5d0626113b7b9e1ad
Noora Panahi <nourap064@gmail.com>
2025-06-08 12:48:34 -0700

    Perk cardsDeck class added

---
+++ perkcardsDeck.hpp
@@ -0,0 +1,16 @@
+#ifndef PERKCARDS_HPP
+#define PERKCARDS_HPP
+#include "perkcards.hpp"
+#include <vector>
+class PerkDeck{
+    private:
+        std::vector<Perkcards> cards; 
+    public:
+        PerkDeck() ; 
+        Perkcards get_random_card(); 
+        void do_the_card() ; 
+
+};
+
+
+#endif
