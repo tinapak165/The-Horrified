c2f9d216244a0f740e57a5080afcd1ef580cfe00
Noora Panahi <nourap064@gmail.com>
2025-06-08 14:47:46 -0700

    working on Hero classes

--- Hero.hpp
+++ Hero.hpp
@@ -1,7 +1,9 @@
 #ifndef HERO_H
 #define HERO_H
 #include <iostream>
+//#include "location.hpp"
 class Hero{
+    friend std::ostream& operator << (std::ostream &out , const Hero &hero) ; 
 
     private:
         std::string name ; 
@@ -11,30 +13,24 @@ class Hero{
         std::string StartingLocation ;
         std::string currentLocation;
 
-
     public:
-        Hero(std::string name , int MaxActions , std::string StartingLocation) ;
-        virtual void DoTheSpecialAction() = 0 ; //do the special action
-        virtual void DisplayInfo() const = 0 ; //displaying info for each hero
-        virtual void MoveTo(std::string Newlocation) ; //change the hero placement
+        Hero(std::string name , int MaxActions , std::string StartingLocation , std::string specialAction) ;
+        Hero() ; 
+        //virtual void DoTheSpecialAction() = 0 ; //do the special action
+        //virtual void DisplayInfo() const = 0 ; //displaying info for each hero
+        void MoveTo(std::string Newlocation) ; //change the hero placement
 
-        virtual std::string GetName()const ; 
-        virtual int GetMaxAction() const  ; // return the number of action each hero have
-        virtual int GetRemainingActions()const ;
-        virtual std::string GetSpecialActionInfo() const ;
-        virtual std::string GetCurrentLocation() const ; 
+        std::string GetName()const ; 
+        int GetMaxAction() const  ; // return the number of action each hero have
+        int GetRemainingActions()const ;
+        std::string GetSpecialActionInfo() const ;
+        std::string GetCurrentLocation() const ; 
         
-        virtual void SetName(const std::string& newName) ; 
-        virtual void SetMaxActions(int newMax) ; 
-        virtual void SetRemainingActions(int newRemaining) ;
-        virtual void SetCurrentLocation(const std::string& location) ;
-        virtual void SetSpecialActionInfo(const std::string spe) = 0 ;
 
+        void SetMaxActions(int newMax) ; 
+        void SetRemainingActions(int newRemaining) ;
+        void SetCurrentLocation(std::string location) ;
+        //virtual void SetSpecialActionInfo(const std::string spe) = 0 ;
 };
-
-
-
-
-
 
 #endif 
