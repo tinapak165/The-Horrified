f67b19247b7b257b7b069856d922da4e460cc006
Noora Panahi <nourap064@gmail.com>
2025-06-08 08:32:53 -0700

    using Cmake

---
+++ build/CMakeFiles/Hero.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hero.dir/Archaeologist.cpp.obj"
+  "CMakeFiles/Hero.dir/Archaeologist.cpp.obj.d"
+  "CMakeFiles/Hero.dir/Hero.cpp.obj"
+  "CMakeFiles/Hero.dir/Hero.cpp.obj.d"
+  "CMakeFiles/Hero.dir/Mayor.cpp.obj"
+  "CMakeFiles/Hero.dir/Mayor.cpp.obj.d"
+  "Hero.exe"
+  "Hero.exe.manifest"
+  "Hero.pdb"
+  "libHero.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hero.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Hero.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Hero.
+# This may be replaced when dependencies are built.
