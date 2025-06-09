f07f1320630514d03968871d93417618ab617e3d
Noora Panahi <nourap064@gmail.com>
2025-06-09 01:04:53 -0700

    added new functions + cleaned

--- Archaeologist.cpp
+++ Archaeologist.cpp
@@ -3,10 +3,10 @@
 using namespace std; 
 
 Archaeologist:: Archaeologist(): Hero("Archaeologist" , 4 , "Docks" , "Can take any number of items from adjacent locations."){}
+
 void Archaeologist:: DisplayInfo() const{
     cout << GetName() << ":\n" << "location: " << GetCurrentLocation() << "\n" << "Actions: " << 
     GetRemainingActions() << "/" << 4 << "\n" << "special Action: " << GetSpecialActionInfo() << "\n"  ; 
 } 
-
 
 
