94505fce023f58b3210ba9cfcd41912bbf0b601a
Noora Panahi <nourap064@gmail.com>
2025-06-09 01:05:54 -0700

    added a new function

--- Mayor.cpp
+++ Mayor.cpp
@@ -3,6 +3,7 @@
 using namespace std ; 
 
 Mayor::Mayor() : Hero("Mayor" , 5 , "Theatre" , "No special action.") {}
+
 void Mayor::DisplayInfo()const {
     cout << GetName() << ":\n" << "location: " << GetCurrentLocation() << "\n" << "Actions: " << 
     GetRemainingActions() << "/" << 5 << "\n" << "special Action: " << GetSpecialActionInfo() << "\n"  ; 
