c98f05bfaa1e7f8a39e84387c3a3b62be5846bdc
Noora Panahi <nourap064@gmail.com>
2025-06-11 08:22:10 -0700

    Dice changed

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(){
     srand(time(0)) ;
 
     Dice d(3) ; 
-    vector<DiceFace> res =  d.roll(3); 
+    vector<DiceFace> res =  d.roll(2); 
     //how to use:
     for(DiceFace e : res){
         switch (e)
