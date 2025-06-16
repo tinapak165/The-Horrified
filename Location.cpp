1c6b8437ad049bcd8041e3b3843a5618fc8b71d2
Noora Panahi <nourap064@gmail.com>
2025-06-16 10:06:21 -0700

    needed location class

---
+++ Location.cpp
@@ -0,0 +1,95 @@
+#include "location.hpp"
+#include <iostream>
+
+    Location::Location(const std::string& name ) : name(name){}
+
+    void Location::add_item(const Item& item) {
+    items.push_back(item);
+}
+
+    void Location::add_hero(Hero* hero){
+    heroes.push_back(hero);
+}
+
+    void Location::add_monster(Monster* monster) {
+    monsters.push_back(monster);
+}
+void Location::remove_hero(Hero* hero) {
+    //heroes.erase(std::remove(heroes.begin(), heroes.end(), hero), heroes.end());
+    for (auto it = heroes.begin(); it != heroes.end(); ) {
+    if (*it == hero) {
+        it = heroes.erase(it);
+    } else {
+            ++it;
+        }
+    }
+}
+void Location::remove_monster(Monster* monster) {
+    //monsters.erase(std::remove(monsters.begin(), monsters.end(), monster), monsters.end());
+    for (auto it = monsters.begin(); it != monsters.end(); ) {
+    if (*it == monster) {
+        it = monsters.erase(it);
+    } else {
+            ++it;
+        }
+    }
+}
+
+
+    const std::string& Location::get_name() const { return name;
+}
+
+     std::vector<Item>& Location::get_items()  {
+    return items;
+}
+
+    void Location::remove_item_by_index(int index) {
+    if (index >= 0 && index < items.size()) {
+        items.erase(items.begin() + index); }
+    }
+    
+    void Location::connect(Location * other){
+        neighbors.push_back(other);
+        other->neighbors.push_back(this);
+        
+    }
+
+     std::vector<Monster*>& Location::get_monsters(){
+        return monsters;
+    }
+   
+    
+     std::vector<Hero*>& Location::get_heroes() {
+        return heroes;
+    }
+    
+
+
+
+const std::vector<Location*>& Location::get_neighbors() const {
+    return neighbors;
+}
+
+
+
+
+bool Location::has_coffin(){   
+    return hascoffin;
+}
+void Location::place_coffin(){ 
+     hascoffin = true;
+}
+void Location::remove_coffin(){
+    hascoffin = false;
+}
+
+
+
+
+ std::ostream operator <<( std::ostream &op , std::vector<Monster*> & monsters){
+
+    op<<monsters;
+
+
+
+}
---
+++ location.hpp
@@ -0,0 +1,50 @@
+#ifndef LOCATION_HPP
+#define LOCATION_HPP
+#include <string>
+#include <vector>
+#include "item.hpp"
+#include "monster.hpp"
+#include "hero.hpp"
+class Monster;
+
+class Location {
+    private:
+        std::string name;
+        std::vector<Item> items;
+        std::vector<Hero*> heroes;
+        std::vector<Monster*> monsters;
+        std::vector<Location*> neighbors;
+        bool hascoffin = false; // فقط واسio دراکولاس
+        
+        public:
+        friend std::ostream operator <<( std::ostream &output , std::vector<Monster*> &);
+        Location(const std::string & name);
+    
+        void add_item(const Item& item);
+        void add_hero(Hero* hero);
+        void remove_hero(Hero* hero) ; 
+        void remove_monster(Monster* monster) ; 
+
+        void add_monster(Monster* monster);
+        bool has_coffin();
+        void place_coffin();
+        void remove_coffin();
+    
+        const std::string& get_name() const;
+         std::vector<Item>& get_items() ; 
+        
+        std::vector<Monster*>& get_monsters();
+        
+
+         std::vector<Hero*>& get_heroes();
+       
+        const std::vector<Location*>& get_neighbors() const;
+        
+        void connect(Location * other);
+        
+        
+        void remove_item_by_index(int index); // برای PickUp
+    
+    };
+    
+#endif
