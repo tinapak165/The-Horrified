560e86733576fddb4b73e6eee4382909c799dbe2
Noora Panahi <nourap064@gmail.com>
2025-06-21 08:07:04 -0700

    everytime checks if any villager in its safeplace

--- play.cpp
+++ play.cpp
@@ -19,6 +19,16 @@ int main(){
     GameMap map ; 
     map.build_map() ;
 
+    villager DrCranley("Dr.Cranley" , map.find_location_by_name("Precinct")) ;  
+    villager DrReed("Dr.Reed" , map.find_location_by_name("Camp")) ;
+    villager ProfPearson("Prof.Pearson" , map.find_location_by_name("Museum")) ; 
+    villager Maleva("Maleva" , map.find_location_by_name("Shop"));
+    villager Fritz("Fritz" , map.find_location_by_name("Institute")) ; 
+    villager WillburChick("Willbur & Chick" , map.find_location_by_name("Dungeon")) ;
+    villager Maria("Maria" , map.find_location_by_name("Camp")) ;
+
+    //Maria.MoveTo(map.find_location_by_name("Camp") , "Maria") ; moving to its safeplace for checking
+
     Mayor m(map) ; 
     m.DisplayInfo() ;
     cout << endl ; 
@@ -27,50 +37,53 @@ int main(){
 
     PerkDeck p ; 
     Perkcards p2 ; 
+    //if on the hero's turn villager moved to its safeplace, play perkcard
+    if(villager::AnyVillagerInSafePlace()){
+        p2 = p.get_random_card() ;
+        cout << p2 << endl;
+        if(p2.get_Event() == "Hurry."){
+            string firstMove , secondMove ; 
+            p.display_the_card(p2) ; 
+            cout << "Mayor..where do you want to move first? " ;
+            cin >> firstMove ; 
+            Location* currentLoc = m.GetCurrentLocation() ; 
+            Location* firstMoveLoc = map.find_location_by_name(firstMove);
+            if(auto* dest = currentLoc->findNeighbor(firstMove)) // اگر محلی که انتخاب شده همسایه بود
+                m.MoveTo(firstMoveLoc) ;        
+            else cerr << "what you have chosen is not a neighboring place!!\n" ;
 
-    p2 = p.get_random_card() ;
-    cout << p2 << endl;
-    if(p2.get_Event() == "Hurry."){
-        string firstMove , secondMove ; 
-        p.display_the_card(p2) ; 
-        cout << "Mayor..where do you want to move first? " ;
-        cin >> firstMove ; 
-        Location* currentLoc = m.GetCurrentLocation() ; 
-        Location* firstMoveLoc = map.find_location_by_name(firstMove);
-        if(auto* dest = currentLoc->findNeighbor(firstMove)) // اگر محلی که انتخاب شده همسایه بود
-            m.MoveTo(firstMoveLoc) ;        
-        else cerr << "what you have chosen is not a neighboring place!!\n" ;
+            cout << "Mayor..what is your second place to move? " ;
+            cin >> secondMove ;
+            Location* secondMoveLoc = map.find_location_by_name(secondMove);
+            if(auto* dest = currentLoc->findNeighbor(secondMove)) // اگر محلی که انتخاب شده همسایه بود
+                m.MoveTo(secondMoveLoc) ;        
+            else cerr << "what you have chosen is not a neighboring place!!\n" ; 
 
-        cout << "Mayor..what is your second place to move? " ;
-        cin >> secondMove ;
-        Location* secondMoveLoc = map.find_location_by_name(secondMove);
-        if(auto* dest = currentLoc->findNeighbor(secondMove)) // اگر محلی که انتخاب شده همسایه بود
-            m.MoveTo(secondMoveLoc) ;        
-        else cerr << "what you have chosen is not a neighboring place!!\n" ; 
+            cout << "--------------\n" ;
+            string AfirstMove , AsecondMove ; 
+            cout << "Archaeologist..where do you want to move first? " ;
+            cin >> AfirstMove ; 
+            Location* AcurrentLoc = a.GetCurrentLocation() ; 
+            Location* AfirstMoveLoc = map.find_location_by_name(AfirstMove);
+            if(auto* dest = AcurrentLoc->findNeighbor(AfirstMove)) // اگر محلی که انتخاب شده همسایه بود
+                a.MoveTo(AfirstMoveLoc) ;        
+            else cerr << "what you have chosen is not a neighboring place!!\n" ;
 
-        cout << "--------------\n" ;
-        string AfirstMove , AsecondMove ; 
-        cout << "Archaeologist..where do you want to move first? " ;
-        cin >> AfirstMove ; 
-        Location* AcurrentLoc = a.GetCurrentLocation() ; 
-        Location* AfirstMoveLoc = map.find_location_by_name(AfirstMove);
-        if(auto* dest = AcurrentLoc->findNeighbor(AfirstMove)) // اگر محلی که انتخاب شده همسایه بود
-            a.MoveTo(AfirstMoveLoc) ;        
-        else cerr << "what you have chosen is not a neighboring place!!\n" ;
+            cout << "Archaeologist..what is your second place to move? " ;
+            cin >> AsecondMove ;
+            Location* AsecondMoveLoc = map.find_location_by_name(AsecondMove);
+            if(auto* dest = AcurrentLoc->findNeighbor(AsecondMove)) // اگر محلی که انتخاب شده همسایه بود
+                a.MoveTo(AsecondMoveLoc) ;        
+            else cerr << "what you have chosen is not a neighboring place!!\n" ;     
+        }
+        else if (p2.get_Event() == "Repel."){}
 
-        cout << "Archaeologist..what is your second place to move? " ;
-        cin >> AsecondMove ;
-        Location* AsecondMoveLoc = map.find_location_by_name(AsecondMove);
-        if(auto* dest = AcurrentLoc->findNeighbor(AsecondMove)) // اگر محلی که انتخاب شده همسایه بود
-            a.MoveTo(AsecondMoveLoc) ;        
-        else cerr << "what you have chosen is not a neighboring place!!\n" ;     
+        else if(p2.get_Event() == "Late into the Night."){
+            //فرض کنیم بازیکنی که نوبتشه شهردار هست (!fix!)
+            p.display_the_card(p2) ;
+            m.SetRemainingActions(m.GetRemainingActions() + 2) ;
+            m.DisplayInfo() ; 
+        }
     }
-    else if (p2.get_Event() == "Repel."){}
-
-    else if(p2.get_Event() == "Late into the Night."){
-        //فرض کنیم بازیکنی که نوبتشه شهردار هست (!fix!)
-        p.display_the_card(p2) ;
-        m.SetRemainingActions(m.GetRemainingActions() + 2) ;
-        m.DisplayInfo() ; 
-    }
+    
 }
