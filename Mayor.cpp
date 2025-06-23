a28b0762b774da588bc0374c2344aa61e129d597
Noora Panahi <nourap064@gmail.com>
2025-06-23 08:54:15 -0700

    location updated

--- Mayor.cpp
+++ Mayor.cpp
@@ -2,7 +2,9 @@
 #include <iostream>
 using namespace std ; 
 
-Mayor::Mayor(GameMap& Map) : Hero("Mayor" , 5 , Map.get_location_by_name("Theater") , "No special action.") {}
+Mayor::Mayor(GameMap& Map) : Hero("Mayor" , 5 , Map.get_location_by_name("Theater") , "No special action.") {
+    Map.get_location_by_name("Theater")->add_hero(this) ; 
+}
 
 void Mayor::DisplayInfo()const {
     cout << GetName() << ":\n" << "location: " << *GetCurrentLocation() << "\n" << "Actions: " << 
