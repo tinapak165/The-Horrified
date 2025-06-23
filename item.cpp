e788a642e75b3754beef338d4cc54edaf6f729da
Noora Panahi <nourap064@gmail.com>
2025-06-23 01:37:47 -0700

    fixed the errors that was showing

--- item.cpp
+++ item.cpp
@@ -3,9 +3,8 @@
 Item::Item(const std::string& name, ItemColor color, int strength, const std::string& locationName)
 : name(name), color(color), strength(strength), locationName(locationName) {}
 
- const std::string& Item::getName() const{ return name;}
- ItemColor Item::getColor() const{ return color;}
- int Item::getStrength() const{ return strength;}
+const std::string& Item::getName() const{ return name;}
+ItemColor Item::getColor() const{ return color;}
+int Item::getStrength() const{ return strength;}
 
-
-
+const std::string &Item::getLocationName() const{ return locationName ; }
