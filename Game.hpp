4de0ce0957cc4945a1134bc13013cdcc696169f5
Noora Panahi <nourap064@gmail.com>
2025-06-24 11:45:44 -0700

    needed these class

---
+++ Game.hpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <unordered_map>
+#include "GameMap.hpp"
+#include "MonsterCardDeck.hpp"
+#include "TurnManager.hpp"
+#include "Monster.hpp"
+#include "Dracula.hpp"
+#include "InvisibleMan.hpp"
+#include "Hero.hpp"
+#include "Dice.hpp"
+#include "Itembag.hpp"
+
+class Game {
+private:
+    GameMap map;
+    MonsterCardDeck deck;
+    TurnManager turnManager;
+    ItemPool pool;
+    std::unordered_map<MonsterType, Monster*> monstersMap;
+    int terrorLevel = 0;
+    
+public:
+    Game();
+
+    void start() ;
+    void hero_phase(Hero* hero);
+    void monster_phase();
+
+    bool both_monsters_defeated();
+
+
+
+    void start() ;
+
+    void distribute_initial_items();
+};
