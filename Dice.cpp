1000e657abd499afe804cac8b4e28ab3b0ad7c38
Noora Panahi <nourap064@gmail.com>
2025-06-24 12:34:11 -0700

    needed these classes

---
+++ Dice.cpp
@@ -0,0 +1,71 @@
+#include "Dice.hpp"
+#include <stdexcept>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <ctime>
+using namespace std ; 
+Dice::Dice(int c) : count(c){
+    
+    faces[1] = {
+        DiceFace::Power , 
+        DiceFace::empty ,
+        DiceFace::empty ,
+        DiceFace::empty ,
+        DiceFace::empty ,
+        DiceFace::empty 
+    }; 
+    faces[2] = {
+        DiceFace::Attack , 
+        DiceFace::empty ,
+        DiceFace::empty ,
+        DiceFace::empty ,
+        DiceFace::empty ,
+        DiceFace::empty 
+
+    };
+    faces[3] = {
+        DiceFace::Attack , 
+        DiceFace::empty ,
+        DiceFace::empty ,
+        DiceFace::empty ,
+        DiceFace::empty ,
+        DiceFace::empty 
+
+    };
+}
+int Dice::get_count(){
+    return count ; 
+}
+
+vector<DiceFace> Dice::roll(int numOfDice){ //انتخاب index تاس رندوم
+    if(numOfDice <= 0 || numOfDice >= 4){
+        throw out_of_range("invalid") ; 
+    }
+    vector<int>DiceIndices = {1,2,3} ; 
+    std::random_shuffle(DiceIndices.begin() , DiceIndices.end()) ; 
+    vector<DiceFace> res ; 
+
+
+    for(int i = 0 ; i < numOfDice ; i++){
+        int RandomDice = DiceIndices[i] ;
+        int RandomFace = rand() % 6 ; 
+        res.push_back(faces[RandomDice][RandomFace]) ; 
+        cout << "Dice " << RandomDice << " rolled: " ;
+        switch (faces[RandomDice][RandomFace])
+        {
+        case DiceFace::Power : 
+            
+            break;
+        
+        case DiceFace::Attack :
+            
+            break ;
+        case DiceFace::empty:
+            break;
+                     
+        }
+        cout << '\n' ; 
+    }
+    return res ; 
+}
