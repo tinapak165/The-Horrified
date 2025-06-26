3283791caadd14dd3aa0bcc2ba3780f801b52d43
Noora Panahi <nourap064@gmail.com>
2025-06-26 14:24:13 -0700

    updated

---
+++ Game.hpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <unordered_map>
+#include "GameMap.hpp"
+#include "MonsterCardDeck.hpp"
+#include "TurnManager.hpp"
+#include "Monster.hpp"
+#include "Dracula.hpp"
+#include "InvisibleMan.hpp"
+#include "Hero.hpp"
+#include "Dice.hpp"
+#include "Itembag.hpp"
+
+class Game {
+private:
+    GameMap map;
+    MonsterCardDeck deck;
+    TurnManager turnManager;
+
+    Hero* mayor = nullptr;
+    Hero* archaeologist = nullptr;
+    
+    ItemPool pool;
+    std::unordered_map<MonsterType, Monster*> monstersMap;
+    int terrorLevel = 0;
+    bool game_over = false;
+    
+public:
+    Game();
+    ~Game();
+    void test();
+    void start() ;
+    void choose_character();
+    void hero_phase(Hero* hero);
+    void play_hero_Action(Hero*);
+    void playPerkCard(Hero*);
+    void locationOverview() ;
+
+    void monster_phase();
+
+    bool both_monsters_defeated();
+
+    void distribute_initial_items();
+};
