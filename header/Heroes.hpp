bf4bec24290536ebadd9e19413dc5e00d8556bce
NooraPanahi <nourap064@gmail.com>
2025-07-27 01:54:17 +0330

    special action for each hero added only courier left

--- header/Heroes.hpp
+++ header/Heroes.hpp
@@ -2,18 +2,50 @@
 #define HEROES_H
 #include "Hero.hpp"
 #include "TurnManager.hpp"
+
 class Archaeologist : public Hero{
+    private:
+        std::string message;
+        std::string chosenPlace;
+        bool typing = true;
+        bool validInput = false;
+        bool showingMessage = false;
+        double messageStartTime = 0.0;
+        Location* chosenLocation = nullptr;
+        GameMap* map = nullptr;
+        std::vector<Item>* itemList = nullptr;
+        int selectedItemIndex = 0;
+        Location* currentTargetLocation = nullptr;
+        bool pickingItems = false;
+        std::vector<Rectangle> itemHitboxes; 
+        Rectangle doneButtonBox = { 740.0f, 460.0f, 70.0f, 30.0f }; 
+        bool autoCompleteWithoutItems = false;
+
+
     public:
         Archaeologist(GameMap&) ;
+        void StartSpecial(GameMap&) override ;
+        void UpdateSpecial(bool &done) override ;
+        void DrawSpecial() override ;
         void DisplayInfo() const ;
-        void SpecialAction(Location*) override ;        
+        void Special(Location*) override ;        
 };
 
 class Mayor : public Hero{
+
+    private:
+        bool shown = false ; 
+        std::string message ; 
+        double messageStartTime = 0.0;
+
     public:
+
         Mayor(GameMap&) ; 
         void DisplayInfo() const  ; 
-        void SpecialAction(Location*) override ; 
+        void StartSpecial(GameMap&) override ;
+        void Special(Location*) override ; 
+        void UpdateSpecial(bool& done) override ;
+        void DrawSpecial() override ;
 };
 
 class Courier : public Hero{
@@ -22,15 +54,22 @@ class Courier : public Hero{
     public:
         Courier(GameMap& , TurnManager&) ; 
         void DisplayInfo() const  ; 
-        void SpecialAction(Location*) override ;
+        void Special(Location*) override ;
 };
 
 class Scientist : public Hero{
+    private:
+        bool shown = false ; 
+        std::string message ; 
+        double messageStartTime = 0.0;
     public:
         Scientist(GameMap&) ; 
         void DisplayInfo() const  ; 
-        void SpecialAction(Location*) override ;
         int Ability(Item& item) override ; 
+        void StartSpecial(GameMap&) override ;
+        void Special(Location*) override ; 
+        void UpdateSpecial(bool& done) override ;
+        void DrawSpecial() override ;
 };
 
 
--- src/Heroes.cpp
+++ src/Heroes.cpp
@@ -6,7 +6,94 @@ using namespace std ;
 Archaeologist:: Archaeologist(GameMap& Map): Hero("archaeologist" , 4 , Map.get_location_by_name("Docks") , "can take any number of items from adjacent locations." , "../Assets/Heros/Archaeologist.png") {
     Map.get_location_by_name("Docks")->add_hero(this , this->getTexture()) ; 
     loadTexture();
+}
 
+void Archaeologist::StartSpecial(GameMap & m){
+    map = &m;
+    typing = true;
+    showingMessage = false;
+    chosenPlace.clear();
+    message.clear();
+    validInput = false;
+    chosenLocation = nullptr;
+    
+}
+
+void Archaeologist::UpdateSpecial(bool &done){
+
+    Location* heroLoc = GetCurrentLocation();
+
+    if (showingMessage) {
+        if (GetTime() - messageStartTime >= 1.5) {
+            showingMessage = false;
+            message.clear();
+
+            if (autoCompleteWithoutItems) {
+                autoCompleteWithoutItems = false ;
+                done = true ;
+            }
+            else 
+                typing = true;
+        }
+        return ;
+    }
+    if (typing) {
+        int key = GetCharPressed();
+        while (key > 0) {
+            if (key >= 32 && key <= 125)
+                chosenPlace += (char)key;
+            key = GetCharPressed();
+        }
+
+        if (IsKeyPressed(KEY_BACKSPACE) && !chosenPlace.empty()) {
+            chosenPlace.pop_back();
+        }
+
+        if (IsKeyPressed(KEY_ENTER)) {
+            if (heroLoc->findNeighbor(chosenPlace)) {
+                chosenLocation = map->get_location_by_name(chosenPlace);
+                Special(chosenLocation);
+                validInput = true;
+            } else {
+                message = "Invalid neighboring location!";
+                validInput = false;
+                chosenPlace.clear() ;
+            }
+
+            typing = false;
+            showingMessage = true;
+            messageStartTime = GetTime();
+        }
+    } 
+    if (pickingItems && itemList) {
+
+        if (pickingItems && IsMouseButtonPressed(MOUSE_LEFT_BUTTON) &&
+            CheckCollisionPointRec(GetMousePosition(), doneButtonBox)) {
+            
+            pickingItems = false;
+            message = "Finished picking items.";
+            done = true;
+        }
+
+        for (size_t i = 0; i < itemHitboxes.size(); ++i) {
+            if (CheckCollisionPointRec(GetMousePosition(), itemHitboxes[i]) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                GetItems().push_back((*itemList)[i]);
+                message = "Picked up " + (*itemList)[i].getName();
+                itemList->erase(itemList->begin() + i);
+
+                if (itemList->empty()) {
+                    pickingItems = false;
+                    message += ". No more items.";
+                    done = true ;
+                }
+                break;
+            }
+        }
+    }
+    if(done){
+        SetRemainingActions(GetRemainingActions() -1) ;
+        return;
+    }
 }
 
 void Archaeologist::DisplayInfo() const {
@@ -36,48 +123,80 @@ void Archaeologist::DisplayInfo() const {
     DrawText(special.c_str(), textX, textY, 20, WHITE);
 }
 
+void Archaeologist::Special(Location* chosenplace){ //amade sazi
 
-void Archaeologist::SpecialAction(Location* chosenplace){
+    currentTargetLocation = chosenplace ;
 
-    vector<Item>&  ItemsAtLocation= chosenplace->get_items() ;
+    itemList = &chosenplace->get_items() ;
+    
+    itemHitboxes.clear();
+    message.clear() ;
 
-    if(ItemsAtLocation.empty()) {
-        cerr << "no item available in " << chosenplace->get_name() << "!\n"; 
+    if(itemList->empty()) {
+        message = "no item available in " + chosenplace->get_name() ; 
+        pickingItems = false ; 
+        showingMessage = true ;
+        messageStartTime = GetTime() ;
+        autoCompleteWithoutItems = true ;
         return ;  
     }
-    else{
-        int selectedItems = -1 ; 
-        while(!ItemsAtLocation.empty()){
-            cout << "items available in " << chosenplace->get_name() << " :\n" ;
-            for(size_t i = 0 ; i < ItemsAtLocation.size() ; i++)
-                cout << (i+1) << ". " << ItemsAtLocation[i].getName() << 
-                    "( color : " <<ItemsAtLocation[i].color_to_string(ItemsAtLocation[i].getColor()) <<
-                     " , strength: " << ItemsAtLocation[i].getStrength() << ")\n";
-        
-            cout << "enter the item number to pick up(0 to end): " ; 
-            cin >> selectedItems ;
-            if(selectedItems == 0) 
-                return ;
-            if(selectedItems < 1 || selectedItems > ItemsAtLocation.size()){
-                cerr << "invalid selection.try again\n" ;
-                continue;
-            }
-            int index = selectedItems - 1 ;
-            GetItems().push_back(ItemsAtLocation[index]) ;
-            cout << (*this).GetName() << " picked up " << ItemsAtLocation[index].getName() << ".\n";
-            ItemsAtLocation.erase(ItemsAtLocation.begin() + index) ; 
-            if(ItemsAtLocation.empty()){
-            cout << "no more items available in " << chosenplace->get_name() << ".\n" ;
-            break ;
-        }
+    pickingItems = true ;  
+}
+
+void Archaeologist::DrawSpecial() {
+
+    Rectangle infoPanel = {340.0f, 200.0f, 480.0f, 300.0f};
+    DrawRectangleRec(infoPanel, Fade(DARKGRAY, 0.94f));
+    DrawRectangleLinesEx(infoPanel, 2, GRAY);
+
+    int x = infoPanel.x + 20;
+    int y = infoPanel.y + 20;
+
+    DrawText("Enter neighboring location to pick up its items:", x, y, 18, RAYWHITE);
+    y += 30;
+    DrawText(("> " + chosenPlace).c_str(), x, y, 24, BLUE);
+    y += 40;
+
+    if (!message.empty()) {
+        DrawText(message.c_str(), x, y, 18, MAROON);
+        y+= 30 ;
     }
-        for(const auto& i : ItemsAtLocation){
-            GetItems().push_back(i) ;
-            cout << (*this).GetName() << " picked up " << i.getName() << " from location " << chosenplace->get_name() << '\n' ;
+
+    if (pickingItems && itemList) {
+        // Draw "Done" button
+        DrawRectangleRec(doneButtonBox, GRAY);
+        if (CheckCollisionPointRec(GetMousePosition(), doneButtonBox)) {
+            DrawRectangleLinesEx(doneButtonBox, 2, YELLOW);
+        } else {
+            DrawRectangleLinesEx(doneButtonBox, 2, WHITE);
         }
-        ItemsAtLocation.clear() ;
+
+        int textWidth = MeasureText("Done", 20);
+        DrawText("Done", doneButtonBox.x + (doneButtonBox.width - textWidth) / 2,
+                        doneButtonBox.y + 5, 20, BLACK);
+
+        if(itemHitboxes.size() != itemList->size())
+        itemHitboxes.resize(itemList->size()) ;
+
+        for (size_t i = 0; i < itemList->size(); ++i) {
+
+            const Item& it = (*itemList)[i];
+            std::string text = std::to_string(i + 1) + ". " + it.getName() +
+                " (color: " + it.color_to_string(it.getColor()) +
+                ", strength: " + std::to_string(it.getStrength()) + ")";
+            
+            Rectangle box = { (float)x - 5, (float)y - 2, 460.0f, 26.0f };
+            itemHitboxes[i] = box ;
+
+            Color bg = CheckCollisionPointRec(GetMousePosition(), box) ? Fade(LIGHTGRAY, 0.5f) : BLANK;
+            DrawRectangleRec(box, bg);
+            DrawText(text.c_str(), x, y, 18, RAYWHITE);
+            y += 30;
+        }
+
     }
 }
+
 
 Mayor::Mayor(GameMap& Map) : Hero("mayor" , 5 , Map.get_location_by_name("Theatre") , "No special action." , "../Assets/Heros/Mayor.png") {
     Map.get_location_by_name("Theatre")->add_hero(this , this->getTexture()) ; 
@@ -111,8 +230,32 @@ void Mayor::DisplayInfo()const {
     DrawText(special.c_str(), textX, textY, 20, WHITE);
 }
 
-void Mayor::SpecialAction(Location*) {
-    cout << "this hero does not have any special action\n" ; 
+void Mayor::StartSpecial(GameMap &){
+
+    message = "this hero does not have any special action" ;
+    shown = false ;  
+}
+
+void Mayor::Special(Location*){}
+
+void Mayor::UpdateSpecial(bool &done){
+
+    if (!shown) {
+        shown = true;
+        messageStartTime = GetTime();
+    } else if (GetTime() - messageStartTime >= 2.0) {
+        done = true;
+    }
+
+}
+
+void Mayor::DrawSpecial(){
+
+    Rectangle infoPanel = {350, 250, 500, 100};
+    DrawRectangleRec(infoPanel, Fade(DARKGRAY, 0.85f));
+    DrawRectangleLinesEx(infoPanel, 2, RAYWHITE);
+    DrawText(message.c_str(), infoPanel.x + 20, infoPanel.y + 35, 22, WHITE);
+
 }
 
 Courier::Courier(GameMap& Map , TurnManager& m): Hero("courier" , 4 , Map.get_location_by_name("Shop") , "can be moved to a location where one of the other heroes is." , "../Assets/Heros/Courier.png") , turn(m) {
@@ -147,7 +290,7 @@ void Courier::DisplayInfo() const {
     DrawText(special.c_str(), textX, textY, 20, WHITE);
 }
 
-void Courier::SpecialAction(Location *){
+void Courier::Special(Location *){
     auto heroes = turn.get_heroes() ; 
     
     for(size_t i = 0 ; i < heroes.size() ; i++){
@@ -195,14 +338,35 @@ void Scientist::DisplayInfo() const {
     DrawText(special.c_str(), textX, textY, 20, WHITE);
 }
 
-void Scientist::SpecialAction(Location *){
-    cout << "this hero does not have any special action\n" ; 
-}
-
 int Scientist::Ability(Item &item){
     cout << "do you want to boost " << item.getName() << "(yes/no)? " ;
     string ans ; cin >> ans ; 
     if(ans == "yes")
         item.setStrength(item.getStrength() + 1) ; 
     return item.getStrength() ; 
+}
+
+void Scientist::StartSpecial(GameMap &){
+
+    message = "this hero does not have any special action" ;
+    shown = false ; 
+}
+void Scientist::Special(Location *){}
+
+void Scientist::UpdateSpecial(bool &done){
+
+    if (!shown) {
+        shown = true;
+        messageStartTime = GetTime();
+    } 
+    else if (GetTime() - messageStartTime >= 2.0) {
+        done = true;
+    }
+}
+
+void Scientist::DrawSpecial(){
+    Rectangle infoPanel = {350, 250, 500, 100};
+    DrawRectangleRec(infoPanel, Fade(DARKGRAY, 0.85f));
+    DrawRectangleLinesEx(infoPanel, 2, RAYWHITE);
+    DrawText(message.c_str(), infoPanel.x + 20, infoPanel.y + 35, 22, WHITE);
 }
