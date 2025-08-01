02e33689542c0d82431f940ce0e39a400098658d
NooraPanahi <nourap064@gmail.com>
2025-08-01 15:18:04 +0330

    two new functions added

--- header/TurnManager.hpp
+++ header/TurnManager.hpp
@@ -13,6 +13,8 @@ class TurnManager {
         TurnManager(const std::vector<Hero*>& heroes);
     
         Hero* get_active_hero();
+        void set_active_hero(const std::string&) ; 
+        Hero* find_hero_by_name(const std::string&) ;
     
         void next_turn();
 
--- src/TurnManager.cpp
+++ src/TurnManager.cpp
@@ -7,6 +7,24 @@ Hero* TurnManager::get_active_hero() {
     return heroes[current_index];
 }
 
+void TurnManager::set_active_hero(const std::string& name)
+{
+    for(size_t i = 0 ; i < heroes.size() ; i++){
+        if(heroes[i]->GetName() == name){
+            current_index = i ; 
+            return ; 
+        }
+    }
+    std::cerr << "Hero with name '" << name << "' not found in TurnManager!\n";
+}
+Hero* TurnManager::find_hero_by_name(const std::string& name) {
+    for (auto hero : heroes) {
+        if (hero->GetName() == name) 
+            return hero;
+    }
+    return nullptr;
+}
+
 void TurnManager:: next_turn() {
     current_index = (current_index + 1) % heroes.size();
 
