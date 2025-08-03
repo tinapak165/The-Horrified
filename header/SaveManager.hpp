8a26e1989d7f7abdea12d8171696f5f6dbb0b041
NooraPanahi <nourap064@gmail.com>
2025-08-03 15:32:58 +0330

    added a class to manage saving files

---
+++ header/SaveManager.hpp
@@ -0,0 +1,22 @@
+#ifndef SAVE_H
+#define SAVE_H
+#include "Game.hpp"
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <vector>
+
+class SaveManager{
+    private:
+        Game& game ; 
+    public:
+        SaveManager(Game&);
+        void saveGame(const std::string&);
+        bool loadGame(const std::string&);
+        std::string generateNextFile();
+        std::vector<std::string> getFiles();
+        void registerSaveFiles(const std::string&) ;
+
+};
+
+#endif
